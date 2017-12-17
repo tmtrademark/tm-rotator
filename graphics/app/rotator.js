075952ad851f1a11f09b687b88254a4e32fc7202
Toby McKes <toby.mckes@gmail.com>
2017-12-17 00:10:57 -0800

    Fuck yeah

--- graphics/app/rotator.js
+++ graphics/app/rotator.js
@@ -1,29 +1,43 @@
 (function () {
 	'use strict';
-	var $heart = $('#heart');
-	var $slot1 = $('#slot1');
-	var $slot2 = $('#slot2');
-	var $slot3 = $('#slot3');
-	var $slot4 = $('#slot4');
-	
-	var delay_mins = 7;
+	var images;
+	var delay_mins = 0.1;
 	var master_delay = delay_mins * 60 * 1000;
-
-	var $ads = $('.ad');
-	var vid = document.getElementById("video"); 
-
-	$("#video").delay(master_delay).fadeIn( 500, function(){
-		vid.play();
+	var container = $('#slideshow');
+	var transition_speed = 2000;
+	var pause_time = 2000;
+	nodecg.readReplicant('assets:images', 'tm-rotator', value => {
+		images = value;
+		images.forEach( function( el ) {
+			var image = '<img class="slide" src="' + el.url + '" />';
+			container.append( image );
+		});
 	});
-	
-	$("#video").bind("ended", function() {
-		$(this).fadeOut();
-		setTimeout(function(){
-			$("#video").fadeIn(500, function(){
-				vid.play();
+
+	function do_the_thing() {
+		setTimeout( function(){
+			container.cycle({
+				cleartypeNoBg: true,
+				speed: transition_speed,
+				delay: 500,
+				timeout: pause_time,
+				autostop: true,
+				nowrap: true,
+				end: function(options){rotation_end()}
 			});
+			container.fadeIn('fast', function(){})
+		}, 1500);
+	}
+	function rotation_end() {
+		container.fadeOut('slow', function(){
+			container.cycle('destroy');
+			console.log( "Rotation ended. Timing out for " + delay_mins + " minutes;")
+		});
+		setTimeout( function() {
+			console.log( "Starting Rotation;" );
+			do_the_thing();
 		}, master_delay );
-	});
-	
-	
+	}
+	do_the_thing();
+
 })();
